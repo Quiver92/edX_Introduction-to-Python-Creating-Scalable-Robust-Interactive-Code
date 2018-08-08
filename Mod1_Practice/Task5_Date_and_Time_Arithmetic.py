de7fe77960286f3f4504c8c17f68bf52617372f6
Vladyslav Lytovka <w.lytovka@koksztys.pl>
2018-08-08 07:51:03 +0200

    minutes_to_weeks

--- Mod1_Practice/Task5_Date_and_Time_Arithmetic.py
+++ Mod1_Practice/Task5_Date_and_Time_Arithmetic.py
@@ -1,3 +1,10 @@
 #timedelta
 # Write a program to find out how many minutes are in a 4-week period
 # Hint: Use a timedelta object and the total_seconds() method
+from datetime import timedelta
+
+def minutes_in_weeks():
+    w = timedelta(weeks = 4)
+    
+    print(w.total_seconds() // 10080)    
+minutes_in_weeks()

0e875abb351be6641468f677d99ac87b7b6f66d0
Vladyslav Lytovka <w.lytovka@koksztys.pl>
2018-08-13 16:02:53 +0200

    mod_1_practice finished

--- Mod1_Practice/Module_1_Practice.py
+++ Mod1_Practice/Module_1_Practice.py
@@ -46,10 +46,8 @@ def name_generator():
     returns:
          `dir_name`: string containing a valid directory name
     """
-    name = datetime.today().strftime("%m_%d_%Y_"+str(randint(0,1000000)))
-    print(name)
-name_generator()
-
+    dir_name = datetime.today().strftime("%m_%d_%Y_"+str(randint(0,1000000)))
+    return dir_name
 
 def directory_creator(name):
     """
@@ -61,7 +59,7 @@ def directory_creator(name):
     returns:
          NONE
     """
-    #TODO
+    os.mkdir(name)
 
 def create():
     """
@@ -75,4 +73,45 @@ def create():
     returns:
          NONE
     """
-    #TODO
+
+    count = directory_count()
+    while count > 0:
+    	count = count - 1
+    	directory_creator(name_generator())
+# Change working directory to `parent_dir` or `create`
+if("parent_dir" not in os.getcwd()):
+    if os.path.exists("./parent_dir"):
+        print("Changing working dir to parent_dir")
+        os.chdir("parent_dir")
+    else:
+        os.mkdir(os.getcwd() + "./parent_dir")
+        print("Changing working dir to parent_dir")
+        os.chdir("parent_dir")
+else:
+    # so the code can run multiple times 
+    # while directory not ending with 'parent_dir' move up the path ..\
+    while "parent_dir" not in os.getcwd()[-11:]:
+        # move up in dir to find 'parent_dir'
+        os.chdir("..")
+        print("moved up", os.getcwd())
+        
+# print the current working directory (should be "parent_dir")
+print("The current working directory is:", os.getcwd())
+
+# check for randoms_directory if not present, create new
+if os.path.exists(os.getcwd() + "/randoms_directory") != True:
+    os.mkdir("randoms_directory")
+
+# change the current working directory to randoms_directory
+print("Changing working dir to randoms_directory")
+os.chdir("randoms_directory")
+# print the current working directory (should be "randoms_dir")
+print("The current working directory is:", os.getcwd())
+
+# create directories inside "randoms_directory"
+create()
+    
+# list the content of the current directory
+print("Current directory content:", os.listdir())
+
+    

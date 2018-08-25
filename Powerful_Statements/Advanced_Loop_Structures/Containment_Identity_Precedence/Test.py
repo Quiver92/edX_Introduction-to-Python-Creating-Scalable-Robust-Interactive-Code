f04bf6bd1b6b14e65d2ee685e6d281a61962439a
Vladyslav <quiver92@gmail.com>
2018-08-25 19:35:03 +0200

    task1 cont_identity_precedence

--- Powerful_Statements/Advanced_Loop_Structures/Containment_Identity_Precedence/Test.py
+++
@@ -1,9 +0,0 @@
-sentence = "This is a test sentence"
-word1 = "test"
-word2 = "something"
-
-# testing if word1 is a substring of sentence
-if (word1 in sentence):
-    print(word1, "is contained in:", sentence)
-else:
-    print(word1, "is not contained in:", sentence)

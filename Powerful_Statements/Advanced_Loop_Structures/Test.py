68e7e048f828d219254ea8c4f0fecb5dfb038be1
Vladyslav Lytovka <w.lytovka@koksztys.pl>
2018-08-27 08:54:37 +0200

    changes at work

---
+++ Powerful_Statements/Advanced_Loop_Structures/Test.py
@@ -0,0 +1,7 @@
+lst = [14, 6, 5, 10, 6, 9, 33, 103, 21, 55]
+
+for num in lst:
+    # Skip all even numbers
+    if (num % 2 == 0):
+        continue
+    print(num)
---
+++ Powerful_Statements/Containment_Identity_Precedence/Task1.py
@@ -0,0 +1,39 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#List containment
+# [ ] The `records` list contains information about a company's employees
+# each of the elements in `records` is a list containing the name and ID of an employee.
+# Write a program to test if `applicant` is contained in `records` and display an appropriate message
+
+# Records of names and IDs
+records = [['Colette', 22347], ['Skye', 35803], ['Alton', 45825], ['Jin', 24213]]
+applicant = ['Joana', 20294]
+if (applicant in records):
+   print("ok")
+else:
+   print("Bull Shit")
+
+
+#String containment
+# [ ] Write a program to prompt the user for a letter (capital or small) then print if the number is a vowel
+# HINT: Use a string containing all the vowels and the `in` or `not in` operator
+
